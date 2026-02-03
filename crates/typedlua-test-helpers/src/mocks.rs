b14d33ef3d06fe3e4859b3ef44fc6339b00fc151
forge18 <ndurham@gmail.com>
2026-02-03 13:07:02 -0600

    feat(tests): add LSP mock providers and update dependencies

--- crates/typedlua-test-helpers/src/mocks.rs
+++ crates/typedlua-test-helpers/src/mocks.rs
@@ -79,3 +79,334 @@ end"#,
         )
     }
 }
+
+#[cfg(feature = "lsp")]
+mod lsp_mocks {
+    use super::*;
+    use lsp_types::{
+        CodeAction, CodeActionContext, CodeActionResponse, CompletionItem, Diagnostic,
+        DocumentSymbolResponse, FoldingRange, FormattingOptions, Hover, InlayHint, Location,
+        Position, Range, SelectionRange, SemanticToken, SemanticTokensDelta, SignatureHelp, Uri,
+        WorkspaceEdit,
+    };
+    use typedlua_lsp::document::Document;
+    use typedlua_lsp::traits::*;
+
+    #[derive(Debug, Default)]
+    pub struct MockCompletionProvider;
+
+    impl MockCompletionProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl CompletionProviderTrait for MockCompletionProvider {
+        fn provide(&self, _document: &Document, _position: Position) -> Vec<CompletionItem> {
+            vec![]
+        }
+
+        fn resolve(&self, item: CompletionItem) -> CompletionItem {
+            item
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockHoverProvider;
+
+    impl MockHoverProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl HoverProviderTrait for MockHoverProvider {
+        fn provide(&self, _document: &Document, _position: Position) -> Option<Hover> {
+            None
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockDefinitionProvider;
+
+    impl MockDefinitionProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl DefinitionProviderTrait for MockDefinitionProvider {
+        fn provide(
+            &self,
+            _uri: &Uri,
+            _document: &Document,
+            _position: Position,
+        ) -> Option<Location> {
+            None
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockReferencesProvider;
+
+    impl MockReferencesProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl ReferencesProviderTrait for MockReferencesProvider {
+        fn provide(
+            &self,
+            _uri: &Uri,
+            _document: &Document,
+            _position: Position,
+            _include_declaration: bool,
+        ) -> Vec<Location> {
+            vec![]
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockRenameProvider;
+
+    impl MockRenameProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl RenameProviderTrait for MockRenameProvider {
+        fn prepare(&self, _document: &Document, _position: Position) -> Option<Range> {
+            None
+        }
+
+        fn rename(
+            &self,
+            _uri: &Uri,
+            _document: &Document,
+            _position: Position,
+            _new_name: &str,
+        ) -> Option<WorkspaceEdit> {
+            None
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockSymbolsProvider;
+
+    impl MockSymbolsProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl SymbolsProviderTrait for MockSymbolsProvider {
+        fn provide(&self, _document: &Document) -> DocumentSymbolResponse {
+            DocumentSymbolResponse::Nested(vec![])
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockFormattingProvider;
+
+    impl MockFormattingProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl FormattingProviderTrait for MockFormattingProvider {
+        fn format_document(
+            &self,
+            _document: &Document,
+            _options: FormattingOptions,
+        ) -> Option<String> {
+            None
+        }
+
+        fn format_range(
+            &self,
+            _document: &Document,
+            _range: Range,
+            _options: FormattingOptions,
+        ) -> Option<String> {
+            None
+        }
+
+        fn format_on_type(
+            &self,
+            _document: &Document,
+            _position: Position,
+            _ch: &str,
+            _options: FormattingOptions,
+        ) -> Option<String> {
+            None
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockCodeActionsProvider;
+
+    impl MockCodeActionsProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl CodeActionsProviderTrait for MockCodeActionsProvider {
+        fn provide(
+            &self,
+            _uri: &Uri,
+            _document: &Document,
+            _range: Range,
+            _context: CodeActionContext,
+        ) -> Option<CodeActionResponse> {
+            None
+        }
+
+        fn resolve(&self, _item: CodeAction) -> Option<CodeAction> {
+            None
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockSignatureHelpProvider;
+
+    impl MockSignatureHelpProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl SignatureHelpProviderTrait for MockSignatureHelpProvider {
+        fn provide(&self, _document: &Document, _position: Position) -> Option<SignatureHelp> {
+            None
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockInlayHintsProvider;
+
+    impl MockInlayHintsProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl InlayHintsProviderTrait for MockInlayHintsProvider {
+        fn provide(&self, _document: &Document, _range: Range) -> Option<Vec<InlayHint>> {
+            None
+        }
+
+        fn resolve(&self, _item: InlayHint) -> Option<InlayHint> {
+            None
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockSelectionRangeProvider;
+
+    impl MockSelectionRangeProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl SelectionRangeProviderTrait for MockSelectionRangeProvider {
+        fn provide(&self, _document: &Document, _positions: Vec<Position>) -> Vec<SelectionRange> {
+            vec![]
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockFoldingRangeProvider;
+
+    impl MockFoldingRangeProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl FoldingRangeProviderTrait for MockFoldingRangeProvider {
+        fn provide(&self, _document: &Document) -> Vec<FoldingRange> {
+            vec![]
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockSemanticTokensProvider;
+
+    impl MockSemanticTokensProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl SemanticTokensProviderTrait for MockSemanticTokensProvider {
+        fn provide_full(&self, _document: &Document) -> Option<Vec<SemanticToken>> {
+            None
+        }
+
+        fn provide_range(&self, _document: &Document, _range: Range) -> Option<Vec<SemanticToken>> {
+            None
+        }
+
+        fn provide_full_delta(
+            &self,
+            _document: &Document,
+            _previous_result_id: Option<String>,
+        ) -> Option<SemanticTokensDelta> {
+            None
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct MockDiagnosticsProvider;
+
+    impl MockDiagnosticsProvider {
+        pub fn new() -> Self {
+            Self
+        }
+    }
+
+    impl DiagnosticsProviderTrait for MockDiagnosticsProvider {
+        fn provide(&self, _document: &Document) -> Vec<Diagnostic> {
+            vec![]
+        }
+    }
+
+    pub fn create_mock_providers() -> (
+        MockCompletionProvider,
+        MockHoverProvider,
+        MockDefinitionProvider,
+        MockReferencesProvider,
+        MockRenameProvider,
+        MockSymbolsProvider,
+        MockFormattingProvider,
+        MockCodeActionsProvider,
+        MockSignatureHelpProvider,
+        MockInlayHintsProvider,
+        MockSelectionRangeProvider,
+        MockFoldingRangeProvider,
+        MockSemanticTokensProvider,
+        MockDiagnosticsProvider,
+    ) {
+        (
+            MockCompletionProvider::new(),
+            MockHoverProvider::new(),
+            MockDefinitionProvider::new(),
+            MockReferencesProvider::new(),
+            MockRenameProvider::new(),
+            MockSymbolsProvider::new(),
+            MockFormattingProvider::new(),
+            MockCodeActionsProvider::new(),
+            MockSignatureHelpProvider::new(),
+            MockInlayHintsProvider::new(),
+            MockSelectionRangeProvider::new(),
+            MockFoldingRangeProvider::new(),
+            MockSemanticTokensProvider::new(),
+            MockDiagnosticsProvider::new(),
+        )
+    }
+}
